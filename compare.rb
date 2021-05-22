d0c9cd9255294186b879b52b7917807e632ad9f1
Miyu-N <myu.star1217@gmail.com>
2021-05-22 13:47:49 +0000

    練習

--- compare.rb
+++ compare.rb
@@ -3,3 +3,7 @@ puts 'ruby' == 'Ruby'
 puts 'ruby' != 'perl'
 
 puts 'ruby' != 'ruby'
+
+puts 1 < 2
+puts 2 * 3 + 4 * 5 - 6 / 2
+
---
+++ sum.rb
@@ -0,0 +1,14 @@
+n = 1
+# n++
+# nを1増やす（n = n + 1と同じ）
+puts n += 1
+
+# nを1減らす(n = n -1と同じ)
+puts n -= 1
+
+
+number = 3
+
+puts "Number is #{number}"
+
+puts 0.1 * 
